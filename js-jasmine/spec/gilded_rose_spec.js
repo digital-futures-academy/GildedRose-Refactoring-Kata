71394a8df36634f55b8f310ad2dba96944fc0eef
paulinakoz <paulinakozaczek@gmail.com>
2021-03-24 12:25:47 +0100

    Added comments to the source code.

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -4,7 +4,10 @@ describe("Gilded Rose", function() {
 
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    console.log(gildedRose)
     const items = gildedRose.updateQuality();
+    console.log(items)
+    console.log(items[0].name)
     expect(items[0].name).toEqual("foo");
   });
 

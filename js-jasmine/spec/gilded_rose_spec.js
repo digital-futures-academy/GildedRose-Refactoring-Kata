623e7d56c490c81c5ccc6bea99403baecce63819
Josh James <joshjames279@hotmail.com>
2021-03-29 14:53:36 +0100

    Test framework updated

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,10 +1,169 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
-describe("Gilded Rose", function() {
+
+describe("Gilded Rose intial criteria checks:\n", function() {
 
   it("should foo", function() {
+
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+
+    expect(items[0].name).toEqual("foo");
+  });
+
+  it("Quality should be a positive integer:", function() {
+
+    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBeGreaterThan(-1);
+  });
+
+  it("Lower sellIN for standard item at end of day:", function() {
+
+    const gildedRose = new Shop([ new Item("foo", 1, 0) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].sellIn).toEqual(0);
+  });
+
+  it("Lower quality for standard item at end of day:", function() {
+
+    const gildedRose = new Shop([ new Item("foo", 0, 1) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("For a standard item once sell by date passes, quality degrades twice as fast:", function() {
+
+    const gildedRose = new Shop([ new Item("foo", -1, 2) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("Aged brie increases in quality:", function() {
+
+    const gildedRose = new Shop([ new Item('Aged Brie', 1, 1) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(2);
+  });
+
+  it("The quality of an item is never more than 50:", function() {
+
+    const gildedRose = new Shop([ new Item('Aged Brie', 1, 50) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it("The quality of an item cannot start at more than 50:", function() {
+
+    const gildedRose = new Shop([ new Item('foo', 1, 55) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it("Sulfuras has no sellIn:", function() {
+
+    const gildedRose = new Shop([ new Item('Sulfuras, Hand of Ragnaros', 1, 80) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].sellIn).toEqual(1);
+  });
+
+  it("Sulfuras never loses quality:", function() {
+
+    const gildedRose = new Shop([ new Item('Sulfuras, Hand of Ragnaros', 1, 80) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(80);
+  });
+
+  it("Sulfuras always has quality of 80:", function() {
+
+    const gildedRose = new Shop([ new Item('Sulfuras, Hand of Ragnaros', 1, 60) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(80);
+  });
+
+  it("Backstage passes increase in quality by 1 for sellIn larger than 10:", function() {
+
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 11, 1) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(2);
+  });
+
+  it("Backstage passes increase in quality by 2 for sellIn between 6 and 10:", function() {
+
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 6, 1) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(3);
+  });
+
+  it("Backstage passes increase in quality by 3 for sellIn between 1 and 5:", function() {
+
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 1, 1) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(4);
+  });
+
+  it("Backstage passes quality drops to zero after concert:", function() {
+
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 1) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("Backstage passes quality drops to zero after concert 2:", function() {
+
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it("The quality of an item is never more than 50 no.2:", function() {
+
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 2, 49) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(50);
+  });
+
+});
+
+describe("Conjured item checks:\n", function() {
+
+  it("Lower quality by 2 each day:", function() {
+
+    const gildedRose = new Shop([ new Item('conjured', 2, 3) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(1);
+  });
+
+  it("Lower sellIn by 1 each day:", function() {
+
+    const gildedRose = new Shop([ new Item('conjured', 2, 3) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].sellIn).toEqual(1);
+  });
+
+  it("Quality can not go below 0:", function() {
+
+    const gildedRose = new Shop([ new Item('conjured', 2, 0) ]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toEqual(0);
   });
 
 });
--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -12,26 +12,15 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             this.items[i].quality = this.items[i].quality - 1;
           }
-        }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
+            this.backstagePass();
           }
         }
       }
@@ -39,28 +28,49 @@ class Shop {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
+        if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
               }
-            }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+          this.agedBrie();
         }
       }
     }
 
     return this.items;
   }
+
+  backstagePass(){
+    for (var i = 0; i < this.items.length; i++){
+    if (this.items[i].sellIn < 11) {
+      if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+      }
+    }
+    if (this.items[i].sellIn < 6) {
+      if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+      }
+    }
+  }
+    return this.items;
+  }
+
+  agedBrie(){
+    if (this.items[i].quality < 50) {
+      this.items[i].quality = this.items[i].quality + 1;
+    }
+    return this.items;
+  }
+
 }
 module.exports = {
   Item,
   Shop
 }
+

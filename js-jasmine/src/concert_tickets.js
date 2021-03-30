0ece2001ea46c95ae4f7d3b8e48200b0e1d37176
paulinakoz <paulinakozaczek@gmail.com>
2021-03-30 15:47:49 +0200

    Concert tickets source code

--- js-jasmine/src/concert_tickets.js
+++ js-jasmine/src/concert_tickets.js
@@ -1,2 +1,23 @@
-
+class Tickets {
+    constructor(sellIn, quality) {
+      this.sellIn = sellIn
+      this.quality = quality
+    }
   
+    updateQuality() {
+      if (this.sellIn === 0) {
+        this.quality = 0
+      } else if (this.sellIn >= 1 && this.sellIn <= 5 && this.quality < 50) {
+        this.quality += 3
+      } else if (this.sellIn >= 6 && this.sellIn <= 10 && this.quality < 50) {
+        this.quality += 2
+      } else if (this.sellIn > 10 && this.quality < 50) {
+        this.quality += 1
+      } else {
+        this.quality = 50
+      }
+      this.sellIn -= 1
+    }
+}
+  
+module.exports = Tickets

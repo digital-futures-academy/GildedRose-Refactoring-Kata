c8cccb69e9359dbb26764f7433ad059026a445af
AntMousinho <antony.mousinho@hotmail.co.uk>
2021-04-02 19:09:38 +0100

    readme updated

--- js-jasmine/texttest_fixture.js
+++ js-jasmine/texttest_fixture.js
@@ -1,19 +1,17 @@
-const Shop = require("../src/gilded_rose");
-const Item = require('../src/item');
-const updateDict = require('../src/updateDict').updateDict;
+const Shop = require("./src/shop");
+const Item = require('./src/item');
+const updateDict = require('./src/updateDict').updateDict;
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
-  new Item("Elixir of the Mongoose", 5, 7),
+  new Item("Aged Brie", 1, 0),
+  new Item("Elixir of the Mongoose", 5, 1),
   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-
-  // This Conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 39),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 45),
+  new Item("Conjured Mana Cake", 1, 9),
 ];
 
 const days = Number(process.argv[2]) || 2;

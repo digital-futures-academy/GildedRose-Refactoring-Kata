0754d199064259b37f99e999b4658cbf59d413eb
Josh James <joshjames279@hotmail.com>
2021-04-04 17:39:38 +0100

    Regular class

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -1,5 +1,6 @@
 const AgedBrie = require('./aged-brie');
 const BackstagePass = require('./backstage-pass');
+const Regular = require('./regular');
 const Sulfuras = require('./sulfuras');
 
 class Item {
@@ -13,7 +14,8 @@ class Item {
 const Inventory = {
   'Aged Brie': AgedBrie,
   'Backstage passes to a TAFKAL80ETC concert': BackstagePass,
-  'Sulfuras, Hand of Ragnaros': Sulfuras
+  'Sulfuras, Hand of Ragnaros': Sulfuras,
+  'Regular': Regular,
 }
 
 class Shop {
@@ -22,25 +24,10 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === 'Aged Brie' || this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' || this.items[i].name === 'Sulfuras, Hand of Ragnaros'){
-      const itemClass = Inventory[this.items[i].name];
-        const item = new itemClass(this.items[i]);
-        return item.update();
+      const itemClass = Inventory[this.items[i].name] || Inventory['Regular'];
+      const item = new itemClass(this.items[i]);
+      item.update();
       }
-
-      if (this.items[i].quality > 0) {
-        this.items[i].quality = this.items[i].quality - 1;
-      }
-
-      this.items[i].sellIn = this.items[i].sellIn - 1;
-
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].quality > 0) {
-          this.items[i].quality = this.items[i].quality - 1;
-        }
-      }
-    }
-
     return this.items;
   }
 }
--- js-jasmine/src/regular.js
+++ js-jasmine/src/regular.js
@@ -3,7 +3,24 @@ class Regular{
     this.items = item
     }
     
-    update() {}
+    update() {
+        if (this.items.quality < 0) {
+            this.items.quality = 0;
+            }
+        if (this.items.quality > 50){
+            this.items.quality = 50
+        }
+        if (this.items.quality > 0) {
+            this.items.quality--;
+          }
+        this.items.sellIn--;
+        if (this.items.sellIn < 0) {
+            if (this.items.quality > 0) {
+              this.items.quality--;
+            }
+          }
+        return [this.items]
+        }
 
 }
 
